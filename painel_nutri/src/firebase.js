3d8515efa3586bfd3da6782852dc073b0b14d00b
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-08 18:52:16 -0300

    Criar painel_nutri/src/firebase.js

---
+++ painel_nutri/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Credenciais oficiais do seu projeto Nutri Life
+const firebaseConfig = {
+  apiKey: "AIzaSyDDonDjAPJzUQSDN6dBrG4p7fhI6YlqTSY",
+  authDomain: "nutri-life-45b6c.firebaseapp.com",
+  projectId: "nutri-life-45b6c",
+  storageBucket: "nutri-life-45b6c.firebasestorage.app",
+  messagingSenderId: "210549500065",
+  appId: "1:210549500065:android:702ee7b33d41b046e968e0"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta o banco de dados Firestore para usarmos nas telas
+export const db = getFirestore(app);
