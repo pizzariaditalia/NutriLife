17df205bb02fb4771eed7d92663e3d4a70551e64
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-08 19:10:53 -0300

    Atualizar o vite.config.js

--- painel_nutri/vite.config.js
+++ painel_nutri/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Define que o projeto rodará na subpasta para podermos publicar no GitHub Pages de graça futuramente
-  base: './', 
+  // Ajuste cirúrgico para o GitHub Pages ler a rota exata do seu repositório
+  base: '/NutriLife/', 
 })
