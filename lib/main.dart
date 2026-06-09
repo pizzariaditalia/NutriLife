ca8c621bd9484ebc22290f8d10e42d371007177e
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-09 06:27:36 -0300

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,24 @@ import 'core/services/notification_service.dart';
 import 'features/auth/login_screen.dart';
 
 void main() async {
+  // Garante que o motor visual do Flutter está pronto
   WidgetsFlutterBinding.ensureInitialized();
   
-  await Firebase.initializeApp();
-  await NotificationService.inicializar();
+  // 🛡️ BLOCO DE SEGURANÇA 1: Firebase
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    debugPrint('Erro ao inicializar o Firebase: $e');
+  }
+  
+  // 🛡️ BLOCO DE SEGURANÇA 2: Notificações
+  try {
+    await NotificationService.inicializar();
+  } catch (e) {
+    debugPrint('Erro ao inicializar as Notificações: $e');
+  }
 
+  // 🚀 Desenha o aplicativo na tela independentemente de erros ocultos
   runApp(const MyApp());
 }
 
@@ -22,7 +35,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Arial',
-        primarySwatch: Colors.green, // ✅ Corrigido
+        primarySwatch: Colors.green,
         useMaterial3: true,
       ),
       home: const LoginScreen(),
