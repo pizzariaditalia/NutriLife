114048f888401bb5efbf8769013863e480b3fe18
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-09 10:03:41 -0300

    Criar lib/firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyDDonDjAPJzUQSDN6dBrG4p7fhI6YlqTSY',
+      appId: '1:210549500065:android:702ee7b33d41b046e968e0',
+      messagingSenderId: '210549500065',
+      projectId: 'nutri-life-45b6c',
+      storageBucket: 'nutri-life-45b6c.firebasestorage.app',
+    );
+  }
+}
