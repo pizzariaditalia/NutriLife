c8f929fdc79197f0dfa87ccf82af8730fbf398d3
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-11 22:53:11 -0300

    Atualizar o vite.config.js

--- painel_nutri/vite.config.js
+++ painel_nutri/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Ajuste cirúrgico para o GitHub Pages ler a rota exata do seu repositório
-  base: '/NutriLife/', 
+  base: '/NutriLife/nutri-adm/', 
 })
